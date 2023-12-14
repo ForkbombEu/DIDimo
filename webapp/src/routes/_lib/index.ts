2a2397e5966fec5e904b804993bae6a709c0b8c0
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-14 16:47:05 +0100

    feat: allow for url without http and www

--- webapp/src/routes/_lib/index.ts
+++ webapp/src/routes/_lib/index.ts
@@ -1,3 +1,6 @@
 import z from 'zod';
 
-export const schema = z.object({ url: z.string().url() });
+const urlRegex =
+	/^(https?:\/\/(?:www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+
+export const schema = z.object({ url: z.string().regex(urlRegex, 'Invalid URL') });
