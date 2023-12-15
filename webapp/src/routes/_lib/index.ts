fb0221d93437e79d89a69bff5494a28ddbec1ac6
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-15 12:18:30 +0100

    fix: removed duplicate file

--- webapp/src/routes/_lib/index.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod';
-
-const urlRegex =
-	/^(https?:\/\/(?:www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-
-export const schema = z.object({ url: z.string().regex(urlRegex, 'Invalid URL') });
