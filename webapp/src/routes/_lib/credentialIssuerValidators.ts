9b8493be0e0f2331f4ac3e4d663d615a45489747
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-15 14:39:31 +0100

    refactor: created _lib subfolder

---
+++ webapp/src/routes/_lib/credentialIssuerValidators.ts
@@ -0,0 +1,38 @@
+import { CredentialIssuersFeaturesTypeOptions as Feature } from '$lib/pocketbase/types.js';
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+import credentialIssuerSchema from '$lib/openid-vc-typescript-json-schema/openid-credential-issuer/schema.json';
+
+//
+
+type ValidationResult<T> = {
+	data: T;
+	feature: Feature;
+};
+
+export function doesFileExist(response: Response): ValidationResult<Response> {
+	if (response.status === 200) return { data: response, feature: Feature.FILE_EXISTS };
+	throw new Error(Feature.FILE_EXISTS);
+}
+
+export async function getResponseJson(response: Response): Promise<ValidationResult<Response>> {
+	try {
+		return { data: await response.json(), feature: Feature.VALID_JSON };
+	} catch (e) {
+		throw new Error(Feature.VALID_JSON);
+	}
+}
+
+export function validateJson(data: unknown): ValidationResult<unknown> {
+	const ajv = new Ajv({ allErrors: true });
+	addFormats(ajv);
+
+	// @ts-ignore
+	delete credentialIssuerSchema['$schema'];
+
+	const validate = ajv.compile(credentialIssuerSchema);
+	validate(data);
+
+	if (validate.errors) throw new Error(Feature.SCHEMA_COMPLIANT);
+	return { data, feature: Feature.SCHEMA_COMPLIANT };
+}
