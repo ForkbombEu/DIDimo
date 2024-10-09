1dfd790a8871b5c396c504147aeb1a586e3b36fd
Puria Nafisi Azizi <puria@dyne.org>
2024-10-09 19:26:05 +0200

    feat: add matomo to the layout (#4)

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,16 @@
+// https://vitepress.dev/guide/custom-theme
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import matomo from "@datagouv/vitepress-plugin-matomo";
+
+export default {
+  extends: DefaultTheme,
+  enhanceApp: ({ app, router, siteData }) => {
+    matomo({
+      router: router,
+      siteID: 10, // Replace with your site id
+      trackerUrl: "https://matomo.dyne.org/" // Replace with your matomo url
+    })
+  }
+} satisfies Theme
+
