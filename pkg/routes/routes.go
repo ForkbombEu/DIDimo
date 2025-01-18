44a9df1e1e3afd9e88e8208410cb2043ab673fad
FilippoTrotter <129587442+FilippoTrotter@users.noreply.github.com>
2025-01-18 11:48:45 +0100

    test: Go code refactor and test for routes endpoint (#35)
    * refactor: move routes handler functions in internal
    
    * fix: add removed endpoint
    
    * add first test for keypairoom endpoint
    
    * fix: routes tests
    
    * fix: remove useless files and small refactor

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -9,7 +8,6 @@ import (
 	"os"
 
 	"github.com/forkbombeu/didimo/pkg/internal/pb"
-	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -17,77 +15,29 @@ import (
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
-func Setup(app *pocketbase.PocketBase) {
+func bindAppHooks(app core.App) {
 	routes := map[string]string{
 		"/workflows/{path...}":  getEnv("ADDRESS_TEMPORAL", "http://localhost:8080"),
 		"/monitoring/{path...}": getEnv("ADDRESS_GRAFANA", "http://localhost:8085"),
 		"/{path...}":            getEnv("ADDRESS_UI", "http://localhost:5100"),
 	}
-
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		for path, target := range routes {
 			se.Router.Any(path, createReverseProxy(target))
 		}
 
-		se.Router.POST("/api/keypairoom-server", func(e *core.RequestEvent) error {
-			var body map[string]map[string]interface{}
+		se.Router.POST("/api/keypairoom-server", pb.KeypairoomServerHandler(app)).Bind(apis.RequireAuth())
 
-			conf, err := pb.FetchKeypairoomConfig(app)
-			if err != nil {
-				return err
-			}
-
-			err = json.NewDecoder(e.Request.Body).Decode(&body)
-			if err != nil {
-				return err
-			}
-			hmac, err := pb.KeypairoomServer(conf, body["userData"])
-			if err != nil {
-				return err
-			}
-
-			return e.JSON(http.StatusOK, map[string]string{"hmac": hmac})
-		}).Bind(apis.RequireAuth())
-
-		se.Router.GET("/api/did", func(e *core.RequestEvent) error {
-			authRecord := e.Auth
-			if authRecord == nil {
-				return apis.NewForbiddenError("Only auth records can access this endpoint", nil)
-			}
-
-			publicKeys, err := app.FindFirstRecordByFilter("users_public_keys", "owner = {:owner_id}", dbx.Params{"owner_id": authRecord.Id})
-			if err != nil {
-				return apis.NewForbiddenError("Only users with public keys can access this endpoint", nil)
-			}
-
-			conf, err := pb.FetchDidConfig(app)
-			if err != nil {
-				return err
-			}
-
-			did, err := pb.ClaimDid(conf, &pb.DidAgent{
-				BitcoinPublicKey: publicKeys.Get("bitcoin_public_key").(string),
-				EcdhPublicKey:    publicKeys.Get("ecdh_public_key").(string),
-				EddsaPublicKey:   publicKeys.Get("eddsa_public_key").(string),
-				EthereumAddress:  publicKeys.Get("ethereum_address").(string),
-				ReflowPublicKey:  publicKeys.Get("reflow_public_key").(string),
-				Es256PublicKey:   publicKeys.Get("es256_public_key").(string),
-			})
-			if err != nil {
-				return err
-			}
-
-			return e.JSON(http.StatusOK, did)
-		}).Bind(apis.RequireAuth())
+		se.Router.GET("/api/did", pb.DidHandler(app)).Bind(apis.RequireAuth())
 
 		return se.Next()
 	})
+}
+func Setup(app *pocketbase.PocketBase) {
 
+	bindAppHooks(app)
 	pb.HookNamespaceOrgs(app)
 	pb.Register(app)
-
-	// ** BAD LINE **
-	// hooks.Register(app)
 
 	jsvm.MustRegister(app, jsvm.Config{
 		HooksWatch: true,
@@ -96,8 +46,8 @@ func Setup(app *pocketbase.PocketBase) {
 		TemplateLang: migratecmd.TemplateLangJS,
 		Automigrate:  true,
 	})
-}
 
+}
 func createReverseProxy(target string) func(r *core.RequestEvent) error {
 	return func(r *core.RequestEvent) error {
 		targetURL, err := url.Parse(target)
