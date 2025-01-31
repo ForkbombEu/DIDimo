f15dbf97ba5c009986d5d6250a221c8edba96f38
FilippoTrotter <filippotrotter99@gmail.com>
2025-01-31 11:39:36 +0100

    fix: remove useless test flag

--- cmd/credimi/credimi.go
+++ cmd/credimi/credimi.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"os"
 
 	crIss "github.com/forkbombeu/didimo/pkg/credential_issuer"
-	"github.com/forkbombeu/didimo/pkg/credential_issuer/workflow"
-	"github.com/google/uuid"
 	"github.com/spf13/cobra"
-	"go.temporal.io/sdk/client"
 )
 
 //go:generate go run  github.com/atombender/go-jsonschema@latest   -p credentialissuer ../../schemas/openid-credential-issuer.schema.json -o ../../pkg/credential_issuer/openid-credential-issuer.schema.go
 func main() {
 	var outputFile string
-	var startWorkflow bool
 
 	rootCmd := &cobra.Command{
 		Use:   "credimi [url]",
@@ -24,11 +19,6 @@ func main() {
 		Args:  cobra.ExactArgs(1), // Ensure exactly one positional argument is provided
 		RunE: func(cmd *cobra.Command, args []string) error {
 			url := args[0]
-
-			if startWorkflow {
-				// Start the Temporal workflow
-				return runWorkflow(url)
-			}
 
 			// Fetch metadata
 			issuerMetadata, err := crIss.FetchCredentialIssuer(url)
@@ -59,46 +49,9 @@ func main() {
 	}
 
 	rootCmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output destination (e.g., stdout or file path)")
-	rootCmd.Flags().BoolVarP(&startWorkflow, "workflow", "w", false, "Start the workflow to process metadata")
 
 	// Execute the root command
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func runWorkflow(url string) error {
-
-	// Create Temporal client
-	c, err := client.Dial(client.Options{})
-	if err != nil {
-		return fmt.Errorf("unable to create Temporal client: %v", err)
-	}
-	defer c.Close()
-
-	// Set up the workflow input
-	workflowInput := workflow.WorkflowInput{
-		BaseURL: url,
-	}
-
-	// Execute the workflow
-	workflowOptions := client.StartWorkflowOptions{
-		ID:        "credimi-workflow" + uuid.New().String(),
-		TaskQueue: "CredimiTaskQueue",
-	}
-
-	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, workflow.CredentialWorkflow, workflowInput)
-	if err != nil {
-		return fmt.Errorf("failed to start workflow: %v", err)
-	}
-
-	// Wait for the workflow to complete and get the result
-	var result string
-	err = we.Get(context.Background(), &result)
-	if err != nil {
-		return fmt.Errorf("error running workflow: %v", err)
-	}
-
-	fmt.Printf("Workflow completed successfully: %s\n", result)
-	return nil
 }
