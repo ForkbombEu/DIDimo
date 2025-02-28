b58b359aa5b22d81a0db19210456c8809252a31d
Puria Nafisi Azizi <puria@dyne.org>
2025-02-28 05:17:36 +0100

    handle from COOLIFY_FQDN

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-/app/didimo serve --dir /pb_data --https=0.0.0.0:8090 "$COOLIFY_FQDN" &
+/app/didimo serve --dir /pb_data --http=0.0.0.0:8090 "$COOLIFY_FQDN" &
 bun run /app/webapp/build/index.js
