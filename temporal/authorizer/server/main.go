03313f0e4377092f15e2951b6c3596e2dc5d5c49
Jaromil <jaromil@dyne.org>
2025-03-05 11:11:31 +0100

    raw import of example temporal authorizer server

---
+++ temporal/authorizer/server/main.go
@@ -0,0 +1,63 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/server/common/authorization"
+	"go.temporal.io/server/common/config"
+	"go.temporal.io/server/temporal"
+
+	"github.com/forkbombeu/didimo/temporal/authorizer"
+)
+
+func main() {
+
+	cfg, err := config.LoadConfig("development", "./config", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := temporal.NewServer(
+		temporal.ForServices(temporal.DefaultServices),
+		temporal.WithConfig(cfg),
+		temporal.InterruptOn(temporal.InterruptCh()),
+		temporal.WithClaimMapper(func(cfg *config.Config) authorization.ClaimMapper {
+			return authorizer.NewMyClaimMapper(cfg)
+		}),
+		temporal.WithAuthorizer(authorizer.NewMyAuthorizer()),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("All services are stopped.")
+}
