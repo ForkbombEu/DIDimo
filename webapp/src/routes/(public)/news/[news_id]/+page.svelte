1ea5935723246ea2cf2dd91827c328fbe41f0d80
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-03-18 20:26:19 +0100

    feat: single `news` page

---
+++ webapp/src/routes/(public)/news/[news_id]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import PageContent from '$lib/layout/pageContent.svelte';
+	import PageTop from '$lib/layout/pageTop.svelte';
+	import T from '@/components/ui-custom/t.svelte';
+	import { Button } from '@/components/ui/button';
+	import { m } from '@/i18n';
+
+	let { data } = $props();
+	const { news } = $derived(data);
+</script>
+
+<PageTop>
+	<div class="flex flex-col gap-2">
+		<T tag="h1">{news.title}</T>
+		<T tag="p" class="text-card-foreground">{@html news.summary}</T>
+		<T tag="small" class="text-muted-foreground">{new Date(news.updated).toLocaleString()}</T>
+	</div>
+	<div class="flex flex-row items-center justify-start gap-2">
+		<a href={news.diff} target="_blank">
+			<Button size="sm">{m.Differences()}</Button>
+		</a>
+		<a href={news.refer} target="_blank">
+			<Button size="sm">{m.Referer()}</Button>
+		</a>
+	</div>
+</PageTop>
+
+<PageContent class="grow bg-secondary" contentClass="flex gap-12 items-start">
+	<div class="grow space-y-16">
+		<div class="prose max-w-none">
+			{@html news.news}
+		</div>
+	</div>
+</PageContent>
