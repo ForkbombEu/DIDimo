c99e08052fbe270b620458b9baff30719d5efcc6
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-03-26 18:52:45 +0100

    feat: created custom button

---
+++ webapp/src/modules/components/ui-custom/button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { cn } from '@/components/ui/utils';
+	import type { ButtonProps } from '@/components/ui/button';
+	import { Button } from '@/components/ui/button';
+	import { localizeHref } from '@/i18n/paraglide/runtime';
+
+	let { class: className, href, ...rest }: ButtonProps = $props();
+</script>
+
+<Button href={href ? localizeHref(href) : undefined} class={cn(className)} {...rest} />
