89ec3aef3042e54e5809ab0b58f5812d224c6362
puria <puria@dyne.org>
2025-03-26 16:02:00 +0100

    chore: use `go generate` to generate the config files
    closes #97

---
+++ pkg/gen.go
@@ -0,0 +1,3 @@
+package pkg
+
+//go:generate go run ../cmd/template/template.go -c ../schemas/OpenID4VP_Wallet/openID_Foundation-config_wallet.json -d ../schemas/OpenID4VP_Wallet/openID_Foundation-default_wallet.json -i ../schemas/OpenID4VP_Wallet/openID_Foundation-variant_config.json -o ../config_templates/OpenID4VP_Wallet/OpenID_Foundation/
