494abff863c5987d907ddca5200588ec120ed106
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2025-03-27 09:38:10 +0100

    feat: test config parser (#101)
    * feat: conformance-checks/configs/get-configs-templates
    
    * feat: conformance-checks/configs/placeholders-by-filenames
    
    * feat: create collection for store configs values
    
    * fix: keys in placeholders by file should be all in kebabcase
    
    * fix: in get-configs-templates strip also config_templates drom files name

---
+++ pb_migrations/1743048748_created_config_values.js
@@ -0,0 +1,135 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": null,
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1307514812",
+        "max": 0,
+        "min": 0,
+        "name": "field_name",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1904915386",
+        "max": 0,
+        "min": 0,
+        "name": "credimi_id",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "json494360628",
+        "maxSize": 0,
+        "name": "value",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "json"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text3734675810",
+        "max": 0,
+        "min": 0,
+        "name": "template_path",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_2923319880",
+        "hidden": false,
+        "id": "relation518144557",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "test_suite",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_863811952",
+        "hidden": false,
+        "id": "relation2462348188",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "provider",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_2784107861",
+    "indexes": [],
+    "listRule": null,
+    "name": "config_values",
+    "system": false,
+    "type": "base",
+    "updateRule": null,
+    "viewRule": null
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2784107861");
+
+  return app.delete(collection);
+})
