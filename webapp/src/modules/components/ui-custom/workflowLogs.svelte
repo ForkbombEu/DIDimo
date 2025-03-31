7ca43f3df17ab42961faba091e93ba456773ff06
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-03-31 13:47:51 +0200

    feat: show test logs on ui

---
+++ webapp/src/modules/components/ui-custom/workflowLogs.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { pb } from '@/pocketbase/index.js';
+	import { untrack } from 'svelte';
+	import { Info } from 'lucide-svelte';
+	import Alert from '@/components/ui-custom/alert.svelte';
+	import { Badge } from '../ui/badge/index.js';
+	import * as Accordion from '../ui/accordion/index.js';
+
+	let logs = $state<WorkflowLogEntry[]>([]);
+
+	type Props = {
+		workflowId: string;
+	};
+
+	const { workflowId }: Props = $props();
+
+	$effect(() => {
+		untrack(() => {
+			pb.realtime.subscribe(
+				`${workflowId}openid4vp-wallet-logs`,
+				(data: WorkflowLogEntry[]) => {
+					console.log(data);
+					logs = data;
+				}
+			);
+		});
+		return () => {
+			pb.realtime.unsubscribe(`${workflowId}openid4vp-wallet-logs`);
+		};
+	});
+
+	type WorkflowLogEntry = {
+		_id: string;
+		msg: string;
+		src: string;
+		time?: number;
+
+		result?: 'SUCCESS' | 'ERROR' | 'FAILED' | 'WARNING' | 'INFO' | string;
+
+		[key: string]: any;
+	};
+</script>
+
+<div class="flex flex-col gap-2 py-2">
+	{#if logs.length === 0}
+		<Alert variant="info" icon={Info}>
+			<p>Waiting for logs...</p>
+		</Alert>
+	{:else}
+		{#each logs as log}
+			<Accordion.Root type="multiple" class="rounded-md bg-muted px-2">
+				<Accordion.Item value={log._id} class="border-none">
+					<Accordion.Trigger
+						class="flex flex-row items-center justify-start gap-2 hover:no-underline"
+					>
+						{#if log.result}
+							<Badge
+								variant={log.result === 'SUCCESS'
+									? 'default'
+									: log.result === 'ERROR'
+										? 'destructive'
+										: 'outline'}
+							>
+								{log.result}
+							</Badge>
+						{/if}
+						<span>{log.msg}</span>
+						{#if log.time}
+							<p class="text-xs text-muted-foreground">
+								{new Date(log.time).toLocaleString()}
+							</p>
+						{/if}
+					</Accordion.Trigger>
+					<Accordion.Content>
+						<pre
+							class="overflow-x-scroll rounded-md bg-secondary p-2 text-xs">{JSON.stringify(
+								log,
+								null,
+								2
+							)}</pre>
+					</Accordion.Content>
+				</Accordion.Item>
+			</Accordion.Root>
+		{/each}
+	{/if}
+</div>
