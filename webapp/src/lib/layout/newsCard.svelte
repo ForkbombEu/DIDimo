a861a6d89d98ff5456471f3db9ca1ccade2ef561
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-04-02 16:40:27 +0200

    feat: news section (#87)
    * fix: make sure the paraglide folder is empty so the translations are fresh
    
    * fix: ignore everything
    
    * fix: move `news` collection to `pb_schema.json` so that we can generate types
    
    * feat: single `news` page
    
    * feat: `news` list page
    
    * feat: `news` related translations
    
    * feat: `news` section in `home`
    
    * feat: `news` card init
    
    * fix: actually use prose by adding to tailwind plugins
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

---
+++ webapp/src/lib/layout/newsCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import T from '@/components/ui-custom/t.svelte';
+	import { Button } from '@/components/ui/button';
+	import { cn } from '@/components/ui/utils';
+	import { m } from '@/i18n';
+	import type { NewsResponse } from '@/pocketbase/types';
+
+	type Props = {
+		news: NewsResponse;
+		class?: string;
+	};
+
+	const { news, class: className = '' }: Props = $props();
+</script>
+
+<div class={cn('flex flex-col gap-6 text-card-foreground', className)}>
+	<div class="flex flex-col">
+		<T tag="h3" class="block">{news.title}</T>
+		<T tag="small" class="block">{@html news.summary}</T>
+	</div>
+	<T tag="small" class="block font-normal leading-snug">{@html `${news.news.slice(0, 200)}...`}</T
+	>
+	<a href="/news/{news.id}">
+		<Button variant="outline" size="sm">
+			{m.View_more()}
+		</Button>
+	</a>
+</div>
