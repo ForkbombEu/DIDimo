588baf8d93347d14dd57bc4e4a02e9612302fd45
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-07 16:43:00 +0200

    import workflow status

--- webapp/src/routes/my/workflows/+page.svelte
+++ webapp/src/routes/my/workflows/+page.svelte
@@ -5,7 +5,8 @@
 	import A from '@/components/ui-custom/a.svelte';
 	import T from '@/components/ui-custom/t.svelte';
 	import * as Table from '@/components/ui/table/index.js';
-	import { toWorkflowExecution } from '@forkbombeu/temporal-ui';
+	import { toWorkflowStatusReadable, WorkflowStatus } from '@forkbombeu/temporal-ui';
+	import TemporalI18nProvider from './[workflow_id]/[run_id]/components/temporal-i18n-provider.svelte';
 
 	let { data } = $props();
 	const { executions } = $derived(data);
@@ -16,30 +17,37 @@
 	<T tag="h1">Workflows</T>
 </PageTop>
 
-<PageContent class="bg-secondary grow">
-	<Table.Root class="rounded-lg bg-white">
-		<Table.Header>
-			<Table.Row>
-				<Table.Head>Status</Table.Head>
-				<Table.Head>Workflow ID</Table.Head>
-				<Table.Head>Type</Table.Head>
-				<Table.Head class="text-right">Start</Table.Head>
-				<Table.Head class="text-right">End</Table.Head>
-			</Table.Row>
-		</Table.Header>
-		<Table.Body>
-			{#each executions as workflow (workflow.execution.runId)}
-				{@const path = `/my/workflows/${workflow.execution.workflowId}/${workflow.execution.runId}`}
+<TemporalI18nProvider>
+	<PageContent class="bg-secondary grow">
+		<Table.Root class="rounded-lg bg-white">
+			<Table.Header>
 				<Table.Row>
-					<Table.Cell>{workflow.status}</Table.Cell>
-					<Table.Cell class="font-medium">
-						<A href={path}>{workflow.execution.workflowId}</A>
-					</Table.Cell>
-					<Table.Cell>{workflow.type.name}</Table.Cell>
-					<Table.Cell class="text-right">{workflow.startTime}</Table.Cell>
-					<Table.Cell class="text-right">{workflow.endTime}</Table.Cell>
+					<Table.Head>Status</Table.Head>
+					<Table.Head>Workflow ID</Table.Head>
+					<Table.Head>Type</Table.Head>
+					<Table.Head class="text-right">Start</Table.Head>
+					<Table.Head class="text-right">End</Table.Head>
 				</Table.Row>
-			{/each}
-		</Table.Body>
-	</Table.Root>
-</PageContent>
+			</Table.Header>
+			<Table.Body>
+				{#each executions as workflow (workflow.execution.runId)}
+					{@const path = `/my/workflows/${workflow.execution.workflowId}/${workflow.execution.runId}`}
+					{@const status = toWorkflowStatusReadable(workflow.status)}
+					<Table.Row>
+						<Table.Cell>
+							{#if status !== null}
+								<WorkflowStatus {status} />
+							{/if}
+						</Table.Cell>
+						<Table.Cell class="font-medium">
+							<A href={path}>{workflow.execution.workflowId}</A>
+						</Table.Cell>
+						<Table.Cell>{workflow.type.name}</Table.Cell>
+						<Table.Cell class="text-right">{workflow.startTime}</Table.Cell>
+						<Table.Cell class="text-right">{workflow.endTime}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</PageContent>
+</TemporalI18nProvider>
