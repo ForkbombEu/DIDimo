cd2bb34787ae5276117f90d5e6f5181d6c40ada1
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-07 17:50:13 +0200

    setup stuff

--- webapp/src/routes/my/workflows/[workflow_id]/[run_id]/+page.ts
+++ webapp/src/routes/my/workflows/[workflow_id]/[run_id]/+page.ts
@@ -1,4 +1,7 @@
 import { pb } from '@/pocketbase/index.js';
+import type { HistoryEvent } from '@forkbombeu/temporal-ui';
+import { error } from '@sveltejs/kit';
+import { z } from 'zod';
 
 //
 
@@ -7,21 +10,39 @@ export const load = async ({ params, fetch }) => {
 
 	const basePath = `/api/workflows/${workflow_id}/${run_id}`;
 
-	// TODO - Make sure that the result of these requests matches the shape of the data imported from `./components/data.ts`
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	//
+
 	const workflowResponse = await pb.send(basePath, {
 		method: 'GET',
 		fetch
 	});
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const workflowResponseValidation = rawWorkflowResponseSchema.safeParse(workflowResponse);
+	if (!workflowResponseValidation.success) {
+		error(500, { message: 'Failed to parse workflow response' });
+	}
+
+	//
+
 	const historyResponse = await pb.send(`${basePath}/history`, {
 		method: 'GET',
 		fetch
 	});
+	const historyResponseValidation = rawHistoryResponseSchema.safeParse(historyResponse);
+	if (!historyResponseValidation.success) {
+		error(500, { message: 'Failed to parse workflow history response' });
+	}
+
+	//
 
 	return {
 		workflowId: workflow_id,
-		workflowResponse,
-		eventHistory: { history: { events: [] } }
+		workflowResponse: workflowResponseValidation.data,
+		eventHistory: historyResponseValidation.data as HistoryEvent[]
 	};
 };
+
+//
+
+const rawWorkflowResponseSchema = z.record(z.unknown());
+
+const rawHistoryResponseSchema = z.array(z.record(z.unknown()));
