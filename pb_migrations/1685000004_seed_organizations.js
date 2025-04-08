7042dd32fc18b9520134ae22e50b20812bc83f4c
phoebus-84 <ennio@dyne.org>
2025-04-08 15:37:05 +0200

    feat: seeds organizations and feature updateIssuers

---
+++ pb_migrations/1685000004_seed_organizations.js
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2025 Forkbomb BV
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// @ts-check
+/// <reference path="../pb_data/types.d.ts" />
+
+const ORGANIZATIONS_NAME = "organizations";
+
+/**
+ *
+ * @param {excludeHooks<core.App>} app
+ * @returns
+ */
+function addDefaultOrganization(app) {
+    const name = "default";
+    const collection = app.findCollectionByNameOrId(ORGANIZATIONS_NAME);
+    const record = new Record(collection);
+    record.set("name", name);
+    record.set("description", "Default organization");
+    app.save(record);
+}
+
+migrate((app) => {
+    addDefaultOrganization(app);
+});
