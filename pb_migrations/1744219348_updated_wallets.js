35500c6492fca6528ddebc1b0646b6a8af6baac7
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-09 19:55:54 +0200

    feat: reuse headers + migration

---
+++ pb_migrations/1744219348_updated_wallets.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_120182150")
+
+  // update collection data
+  unmarshal({
+    "createRule": "",
+    "listRule": "",
+    "viewRule": ""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_120182150")
+
+  // update collection data
+  unmarshal({
+    "createRule": null,
+    "listRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+})
