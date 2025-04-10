af9e321e9c4b6340eff0aa352941ba64581aa65f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-10 12:12:54 +0200

    refactor: create fetchUserWorkflows function

---
+++ webapp/src/lib/workflows/index.ts
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2025 Forkbomb BV
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { pb } from '@/pocketbase';
+import z from 'zod';
+
+export async function fetchUserWorkflows(fetchFn = fetch) {
+	const data = await pb.send('/api/workflows', {
+		method: 'GET',
+		fetch: fetchFn
+	});
+
+	return genericResponseSchema.safeParse(data);
+}
+
+const workflowExecutionSchema = z.object({
+	execution: z.object({
+		runId: z.string(),
+		workflowId: z.string()
+	}),
+	executionTime: z.string(),
+	memo: z.record(z.unknown()),
+	rootExecution: z.object({
+		runId: z.string(),
+		workflowId: z.string()
+	}),
+	startTime: z.string(),
+	endTime: z.string().optional(),
+	status: z.string(),
+	taskQueue: z.string(),
+	type: z.object({
+		name: z.string()
+	})
+});
+
+const genericResponseSchema = z.object({
+	executions: z.array(workflowExecutionSchema)
+});
--- webapp/src/routes/my/tests/runs/+page.ts
+++ webapp/src/routes/my/tests/runs/+page.ts
@@ -2,19 +2,14 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { pb } from '@/pocketbase';
+import { fetchUserWorkflows } from '$lib/workflows/index.js';
+
 import { error } from '@sveltejs/kit';
-import { z } from 'zod';
 
 //
 
 export const load = async ({ fetch }) => {
-	const data = await pb.send('/api/workflows', {
-		method: 'GET',
-		fetch
-	});
-
-	const workflows = genericResponseSchema.safeParse(data);
+	const workflows = await fetchUserWorkflows(fetch);
 	if (!workflows.success) {
 		error(500, {
 			message: 'Failed to parse response'
@@ -25,27 +20,3 @@ export const load = async ({ fetch }) => {
 		executions: workflows.data.executions
 	};
 };
-
-const workflowExecutionSchema = z.object({
-	execution: z.object({
-		runId: z.string(),
-		workflowId: z.string()
-	}),
-	executionTime: z.string(),
-	memo: z.record(z.unknown()),
-	rootExecution: z.object({
-		runId: z.string(),
-		workflowId: z.string()
-	}),
-	startTime: z.string(),
-	endTime: z.string().optional(),
-	status: z.string(),
-	taskQueue: z.string(),
-	type: z.object({
-		name: z.string()
-	})
-});
-
-const genericResponseSchema = z.object({
-	executions: z.array(workflowExecutionSchema)
-});
