dccbb00c9c3320a60e128ecbc6994aabb3cfb295
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-10 13:09:51 +0200

    feat: custom json highlighting (#138)
    Co-authored-by: Francesco Castrovilli <francesco.castrovilli95@gmail.com>
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

---
+++ webapp/src/modules/forms/fields/placeholderHighlightCodeEditorField.svelte
@@ -0,0 +1,43 @@
+<!--
+SPDX-FileCopyrightText: 2025 Forkbomb BV
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script lang="ts" generics="Data extends GenericRecord">
+	import type { GenericRecord } from '@/utils/types';
+	import * as Form from '@/components/ui/form';
+	import type { FormPathLeaves, SuperForm } from 'sveltekit-superforms';
+	import { formFieldProxy } from 'sveltekit-superforms';
+	import type { ComponentProps } from 'svelte';
+	import FieldWrapper from './parts/fieldWrapper.svelte';
+	import type { FieldOptions } from './types';
+	import PlaceholderHighlightCodeEditor from '@/components/ui-custom/placeholderHighlightCodeEditor.svelte';
+
+	//
+
+	interface Props {
+		form: SuperForm<Data>;
+		name: FormPathLeaves<Data, string | number>;
+		options: Partial<FieldOptions> & ComponentProps<typeof PlaceholderHighlightCodeEditor>;
+		fieldValues?: Record<string, { valid: boolean; value: string }>;
+	}
+
+	const { form, name, options, fieldValues = {} }: Props = $props();
+
+	const { validate } = form;
+	const { value: v } = formFieldProxy(form, name);
+</script>
+
+<Form.Field {form} {name}>
+	<FieldWrapper field={name} {options}>
+		<PlaceholderHighlightCodeEditor
+			{...options}
+			{fieldValues}
+			bind:value={$v as string}
+			onBlur={() => {
+				validate(name);
+			}}
+		/>
+	</FieldWrapper>
+</Form.Field>
