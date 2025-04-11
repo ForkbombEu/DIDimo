bdd28e8cbd8d3ab9f54f6ae9ed087c4cafbb6f4a
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-11 16:28:26 +0200

    Create +page.svelte

---
+++ webapp/src/routes/my/create-new-check/+page.svelte
@@ -0,0 +1,22 @@
+<!--
+SPDX-FileCopyrightText: 2025 Forkbomb BV
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script lang="ts">
+	import { CollectionForm } from '@/collections-components';
+	import CollectionManager from '@/collections-components/manager/collectionManager.svelte';
+</script>
+
+<CollectionManager collection="custom_checks">
+	{#snippet top({ Header })}
+		<Header />
+	{/snippet}
+
+	{#snippet records({ records })}
+		{#each records as record}
+			<pre class="bg-white p-4">{JSON.stringify(record, null, 4)}</pre>
+		{/each}
+	{/snippet}
+</CollectionManager>
