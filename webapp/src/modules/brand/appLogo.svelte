84b612267844ca73300fd378ac89bca58b43520b
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-04-15 13:27:55 +0200

    fix: avatar in profile, logos, layout of the dev dashboard (#176)
    * fix: user avatar upload
    
    * fix: use full logo on footer
    
    * fix: allow users to upload wallet's logo
    
    * fix: remove block borders
    
    * fix: remove enctype
    
    * fix: make sure the overflow happens only on mobile
    
    * fix: use svgs instead of pngs
    
    * fix: check for `conformanceChecks` first
    
    * chore: extended logo in footer in dark version
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- webapp/src/modules/brand/appLogo.svelte
+++ webapp/src/modules/brand/appLogo.svelte
@@ -6,6 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <script lang="ts">
 	import { appName } from '@/brand';
+	import { cn } from '@/components/ui/utils';
 	import { localizeHref } from '@/i18n';
 
 	interface Props {
@@ -13,13 +14,20 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		 * The url to navigate to when the logo is clicked.
 		 */
 		href?: string;
+		version?: 'icon' | 'full';
 	}
 
-	let { href = '/' }: Props = $props();
+	let { href = '/', version = 'icon' }: Props = $props();
+
+	const logo = version === 'icon' ? '/logos/credimi_logo.svg' : '/logos/credimi_logo-transp_white.svg';
 </script>
 
-<a href={localizeHref(href)}>
-	<img src={'/logos/credimi_logo-transp_emblem.png'} class="h-9 w-auto" alt={appName} />
+<a href={localizeHref(href)} class="flex items-center justify-center gap-2">
+	<img
+		src={logo}
+		class={cn('object-cover', version === 'icon' ? 'h-14 w-14' : 'h-10 w-auto')}
+		alt={appName}
+	/>
 </a>
 
 <!--  
--- webapp/src/modules/components/ui-custom/navigationTabs.svelte
+++ webapp/src/modules/components/ui-custom/navigationTabs.svelte
@@ -15,7 +15,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	let { tabs = [] }: Props = $props();
 </script>
 
-<ul class="flex gap-1 overflow-scroll">
+<ul class="flex gap-1 overflow-x-scroll sm:overflow-hidden">
 	{#each tabs as tab}
 		<li role="presentation">
 			<NavigationTab {...tab} />
