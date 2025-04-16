1185fcf0546a7779297f91aabe97945cd005fed4
FilippoTrotter <filippotrotter99@gmail.com>
2025-04-16 12:35:03 +0200

    refactor: remove old useless code

--- pkg/credential_issuer/workflow/shared.go
+++ pkg/credential_issuer/workflow/shared.go
@@ -4,10 +4,7 @@
 
 package workflow
 
-import credentialissuer "github.com/forkbombeu/didimo/pkg/credential_issuer"
-
 const FidesIssuersUrl = "https://credential-catalog.fides.community/api/public/credentialtype?includeAllDetails=false&size=200"
-const CredentialsTaskQueue = "CredentialsTaskQueue"
 
 type FetchIssuersActivityResponse struct{ Issuers []string }
 
@@ -39,92 +36,4 @@ type CreateCredentialIssuersInput struct {
 	DBPath  string
 }
 
-type Credential struct {
-	CredentialDefinition                 *credentialissuer.OpenidCredentialIssuerSchemaJsonCredentialConfigurationsSupportedValueCredentialDefinition                      `json:"credential_definition,omitempty"`
-	CredentialSigningAlgValuesSupported  []credentialissuer.OpenidCredentialIssuerSchemaJsonCredentialConfigurationsSupportedValueCredentialSigningAlgValuesSupportedElem  `json:"credential_signing_alg_values_supported,omitempty"`
-	CryptographicBindingMethodsSupported []credentialissuer.OpenidCredentialIssuerSchemaJsonCredentialConfigurationsSupportedValueCryptographicBindingMethodsSupportedElem `json:"cryptographic_binding_methods_supported,omitempty"`
-	Display                              []credentialissuer.OpenidCredentialIssuerSchemaJsonCredentialConfigurationsSupportedValueDisplayElem                              `json:"display,omitempty"`
-	Format                               string                                                                                                                            `json:"format"`
-	ProofTypesSupported                  credentialissuer.OpenidCredentialIssuerSchemaJsonCredentialConfigurationsSupportedValueProofTypesSupported                        `json:"proof_types_supported,omitempty"`
-	Scope                                *string                                                                                                                           `json:"scope,omitempty"`
-}
-
-type StoreCredentialsActivityInput struct {
-	IssuerData *credentialissuer.OpenidCredentialIssuerSchemaJson
-	IssuerID   string
-	DBPath     string
-	CredKey    string
-	IssuerName string
-	Credential Credential
-}
-
 const FetchIssuersTaskQueue = "FetchIssuersTaskQueue"
-
-const wellKnownJSON = `{
-  "credential_issuer": "https://dev.issuer1.forkbomb.eu/credential_issuer",
-  "credential_endpoint": "https://dev.issuer1.forkbomb.eu/credential_issuer/credential",
-  "authorization_servers": [
-    "https://dev.authz-server1.forkbomb.eu/authz_server"
-  ],
-  "display": [
-    {
-      "name": "Forkbomb Test Issuer",
-      "locale": "en-US"
-    }
-  ],
-  "jwks": {
-    "keys": [
-      {
-        "kid": "did:dyne:sandbox.genericissuer:3suepGGjNHJmGDBebsCmapkdfBfXwFZzEQcEAMu7EdwA#es256_public_key",
-        "crv": "P-256",
-        "alg": "ES256",
-        "kty": "EC"
-      }
-    ]
-  },
-  "credential_configurations_supported": {
-    "discount_from_voucher": {
-      "format": "vc+sd-jwt",
-      "cryptographic_binding_methods_supported": [
-        "jwk",
-        "did:dyne:sandbox.signroom"
-      ],
-      "credential_signing_alg_values_supported": [
-        "ES256"
-      ],
-      "proof_types_supported": {
-        "jwt": {
-          "proof_signing_alg_values_supported": [
-            "ES256"
-          ]
-        }
-      },
-      "display": [
-        {
-          "name": "Get discount from Voucher dev",
-          "locale": "en-US",
-          "logo": {
-            "url": "https://avatars.githubusercontent.com/u/96812851?s=200&v=4",
-            "alt_text": "Get discount from Voucher dev logo",
-            "uri": "https://avatars.githubusercontent.com/u/96812851?s=200&v=4"
-          },
-          "background_color": "#12107c",
-          "text_color": "#FFFFFF",
-          "description": "Get a special discount for all plans of DIDroom! Enter your voucher and get a discount credential."
-        }
-      ],
-      "vct": "discount_from_voucher",
-      "claims": {
-        "has_discount_from_voucher": {
-          "mandatory": true,
-          "display": [
-            {
-              "locale": "en-US",
-              "name": "Has a discount from Voucher"
-            }
-          ]
-        }
-      }
-    }
-  }
-}`
--- pkg/credential_issuer/workflow/workflow_test.go
+++ pkg/credential_issuer/workflow/workflow_test.go
@@ -8,37 +8,10 @@ import (
 	"errors"
 	"testing"
 
-	credentialissuer "github.com/forkbombeu/didimo/pkg/credential_issuer"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.temporal.io/sdk/testsuite"
 )
-
-func Test_Workflow(t *testing.T) {
-	testSuite := &testsuite.WorkflowTestSuite{}
-	env := testSuite.NewTestWorkflowEnvironment()
-	var issuerData credentialissuer.OpenidCredentialIssuerSchemaJson
-	// Mock activity implementation
-	env.OnActivity(FetchCredentialIssuerActivity, mock.Anything, mock.Anything).Return(&issuerData, nil)
-	credential := Credential(issuerData.CredentialConfigurationsSupported["discount_from_voucher"])
-	inputStoreOrUpdate := StoreCredentialsActivityInput{
-		IssuerData: &issuerData,
-		IssuerID:   mock.Anything,
-		DBPath:     mock.Anything,
-		CredKey:    mock.Anything,
-		IssuerName: mock.Anything,
-		Credential: credential,
-	}
-	env.OnActivity(StoreOrUpdateCredentialsActivity, inputStoreOrUpdate).Return(nil)
-	env.OnActivity(CleanupCredentialsActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
-	env.ExecuteWorkflow(CredentialWorkflow, CredentialWorkflowInput{BaseURL: "example@test.com"})
-
-	var result CredentialWorkflowResponse
-
-	assert.NoError(t, env.GetWorkflowResult(&result))
-	assert.Equal(t, "Credentials Workflow completed successfully for URL: example@test.com", result.Message)
-}
 
 func Test_SuccessfulFetchIssuersWorkflows(t *testing.T) {
 	testSuite := &testsuite.WorkflowTestSuite{}
