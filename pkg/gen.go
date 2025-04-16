0a4e333eaa1d083e80a19f50da92f0775be267b7
Puria Nafisi Azizi <puria@dyne.org>
2025-04-17 00:52:40 +0200

    feat: add Ed25519Signature2020 to credential_signing_alg_values_supported

--- pkg/gen.go
+++ pkg/gen.go
@@ -5,3 +5,4 @@
 package pkg
 
 //go:generate go run ../cmd/template/template.go -c ../schemas/OpenID4VP_Wallet/openID_Foundation-config_wallet.json -d ../schemas/OpenID4VP_Wallet/openID_Foundation-default_wallet.json -i ../schemas/OpenID4VP_Wallet/openID_Foundation-variant_config.json -o ../config_templates/OpenID4VP_Wallet/OpenID_Foundation/
+//go:generate go run github.com/atombender/go-jsonschema@v0.18.0 -p credentials_config ../schemas/openid-credential-issuer.schema.json -o workflow_engine/workflows/credentials_config/openid-credential-issuer.schema.go
--- webapp/src/lib/types/openid-credential-issuer.ts
+++ webapp/src/lib/types/openid-credential-issuer.ts
@@ -70,7 +70,15 @@ export interface OpenidCredentialIssuerSchema {
         | "did:key"
         | "cose_key"
       )[];
-      credential_signing_alg_values_supported?: ("ES256" | "EdDSA" | "RS256" | "ES256K" | "RSA" | "RsaSignature2018")[];
+      credential_signing_alg_values_supported?: (
+        | "ES256"
+        | "EdDSA"
+        | "Ed25519Signature2020"
+        | "RS256"
+        | "ES256K"
+        | "RSA"
+        | "RsaSignature2018"
+      )[];
       proof_types_supported?: {
         [k: string]: {
           proof_signing_alg_values_supported: ("ES256" | "EdDSA" | "PS256" | "RS256")[];
