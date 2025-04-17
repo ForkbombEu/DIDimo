e334b394e132f9f7e4a2cbe009c19ec4c1933806
Puria Nafisi Azizi <puria@dyne.org>
2025-04-17 15:41:04 +0200

    chore: better message for pre-release commits

--- .goreleaser-version-hook.sh
+++ .goreleaser-version-hook.sh
@@ -5,13 +5,13 @@ echo "$GORELEASER_VERSION" > VERSION
 
 # Optional: configure git if running in CI (especially GitHub Actions)
 git config user.name "goreleaser"
-git config user.email "goreleaser@example.com"
+git config user.email "goreleaser@forkbomb.eu"
 
 # Add and commit the file
 git add VERSION
 if git diff --cached --quiet; then
     echo "No changes to commit."
 else
-    git commit -m "chore: updat VERSION file [ci skip]"
+    git commit -m "chore(pre-release): 🚀 update VERSION to ${GORELEASER_VERSION} [ci skip]"
     git push origin main
 fi
